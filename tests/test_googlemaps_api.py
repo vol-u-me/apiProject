458490c3c242b097c91d01957e73237a2bf130e0
vol_u_me <volynchick@gmail.com>
2023-02-01 16:24:42 +0200

    full API test fake_googlemaps

--- tests/test_googlemaps_api.py
+++ tests/test_googlemaps_api.py
@@ -1,3 +1,5 @@
+import json
+
 from utils.api import GoogleMapsApi
 from utils.checking import Cheking
 """Create, edit and del new location"""
@@ -12,30 +14,48 @@ class TestCreatePlace:
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Cheking.check_status_code(result_post, 200)
-        print(result_post.status_code)
+        Cheking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        token = json.loads(result_post.text)
+        Cheking.check_json_value(result_post, 'status', 'OK')
+
 
 
         print()
         print('GET method for check creation')
         result_get = GoogleMapsApi.check_create_new_place(place_id)
         Cheking.check_status_code(result_get, 200)
+        Cheking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        Cheking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
+
 
         print()
         print('PUT method')
         result_put = GoogleMapsApi.update_the_place(place_id)
         Cheking.check_status_code(result_put, 200)
+        Cheking.check_json_token(result_put, ['msg'])
+        Cheking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
         print()
         print('GET method for check update')
         result_get = GoogleMapsApi.check_create_new_place(place_id)
         Cheking.check_status_code(result_get, 200)
+        Cheking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        Cheking.check_json_value(result_get, 'address', '100 Baker street, London')
 
         print()
         print('DELETE method')
         result_delete = GoogleMapsApi.delete_the_place(place_id)
         Cheking.check_status_code(result_delete, 200)
+        Cheking.check_json_token(result_delete, ['status'])
+        Cheking.check_json_value(result_delete, 'status', 'OK')
 
         print()
         print('GET method for check deleting')
         result_get = GoogleMapsApi.check_create_new_place(place_id)
         Cheking.check_status_code(result_get, 404)
+        Cheking.check_json_token(result_get, ['msg'])
+        Cheking.check_json_value(result_get, 'msg', "Get operation failed, looks like place_id  doesn't exists")
+
+        print('___________________________________________________')
+        print('create, edit and del new location testing completed')
+
--- utils/checking.py
+++ utils/checking.py
@@ -1,11 +1,28 @@
-
-"""methods for checking responses to requests"""
+import json
 from requests import Response
 
 
+"""methods for checking responses to requests"""
 class Cheking:
     """Status code check"""
     @staticmethod
     def check_status_code(response: Response, status_code):
         assert status_code == response.status_code
         print('Successfully! Status code is: ', response.status_code)
+
+
+    """method to check for required fields"""
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print('all required fields are in JSON')
+
+
+    """Method for checking the contents of fields"""
+    @staticmethod
+    def check_json_value(response: Response, field_name, expected_value):
+        check = response.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print(field_name, 'is correct!')
